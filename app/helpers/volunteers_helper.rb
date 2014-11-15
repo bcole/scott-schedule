02cafab3f84ad4eec06dd6cf46f35d06dd008f6c
bcole <333qaz333@gmail.com>
2014-11-15 14:16:36 -0500

    Bug fix correcting time that is displayed on the modal popup.

--- app/helpers/volunteers_helper.rb
+++ app/helpers/volunteers_helper.rb
@@ -23,13 +23,19 @@ module VolunteersHelper
 
 		time = block.start_time
 
+
 		start_slot = 10
-        t = start_slot + time
+        t = start_slot + (time / 2)
         s = 'AM'
         if t > 11
         	s = 'PM'
 		end
         t = (t - 1) % 12 + 1
-        d + ' - ' + t.to_s + s
+
+        str = ":00"
+        if (time % 2 == 1)
+        	str = ":30"
+        end
+        d + ' - ' + t.to_s + str + s
 	end
 end
